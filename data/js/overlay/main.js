2044547141848dc5c6d49d915de5ace7533a420a
Lukáš Jech <klukule@moowdesign.eu>
2016-03-18 20:50:34 +0100

    Overlay background fix

--- data/js/overlay/main.js
+++ data/js/overlay/main.js
@@ -23,9 +23,13 @@ function Initialize(){
 function UpdateSettings(){
   RegisterGlobalHotKey(Data.settings.shortcut);
   if(Data.settings.background){
-
+    $('html').css("background","radial-gradient(ellipse at center, "+Data.settings.bg1+" 0%,"+Data.settings.bg2 + " 100%");
+    $("#buttonSettings").removeClass("custom-nobg");
+    $("#buttonClose").removeClass("custom-nobg");
   }else{
-    
+  	$('html').css("background","rgba(0,0,0,0)"); //Transparent background
+  	$("#buttonSettings").addClass("custom-nobg");
+  	$("#buttonClose").addClass("custom-nobg");
   }
 }
 
